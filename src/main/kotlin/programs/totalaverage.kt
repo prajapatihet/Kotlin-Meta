c1e5f90bfbd88e5a92b7d8202840a07d427e85c5
Het Prajapati <hetprajapati80@gmail.com>
2023-07-08 21:12:12 +0530

    Initial Commit

---
+++ src/main/kotlin/programs/totalaverage.kt
@@ -0,0 +1,11 @@
+package programs;
+fun main(){
+    val scoreLevel1 = 79
+    val scoreLevel2 = 92
+    val scoreLevel3 = 86
+
+    val totalScore = scoreLevel1 + scoreLevel2 + scoreLevel3
+    val averageScore = totalScore / 3
+    println(totalScore)
+    println(averageScore)
+}
---
+++ src/main/kotlin/programs/txt.kt
@@ -0,0 +1,7 @@
+package programs
+fun main(){
+    val c1 = 'A'
+    println(c1.code)
+    val c2 = 'B'
+    println(c2.code)
+}
